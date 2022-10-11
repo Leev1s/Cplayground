b26a4d28f9bc408532ee39e9e72f00cec90696b5
Lev1s <lev1sstudio.cn@gmail.com>
2022-10-11 14:31:08 +0800

    14:31

---
+++ wow.c
@@ -0,0 +1,25 @@
+/*
+@File    :   wow.c
+@Time    :   2022/10/11 14:18:04
+@Author  :   Lev1s
+@Version :   1.0
+@Contact :   Lev1sStudio.cn@gmail.com
+@PW      :   http://Lev1s.cn
+@Github  :   https://github.com/o0Lev1s0o
+    __             ___           ______
+   / /   ___ _   _<  /____      / ____/
+  / /   / _ \ | / / / ___/_____/ /     
+ / /___/  __/ |/ / (__  )_____/ /___   
+/_____/\___/|___/_/____/      \____/                                      
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+
+
+int main(void){
+
+    getchar();
+}
